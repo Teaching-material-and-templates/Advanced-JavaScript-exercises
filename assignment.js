af2cf77f6569ce2f86017ac2eaedcce7f177e5e3
cph-mtnl <mtnl@cphbusiness.dk>
2022-01-25 14:44:35 +0100

    assignment

---
+++ assignment.js
@@ -0,0 +1,17 @@
+console.log('Running assignment...');
+
+const transactions = require('./simple-transaction-data').data;
+
+// Assignment 1:
+const filterFunction = (data) => {
+    return data.filter((transaction) => {
+        // TODO
+    });
+}
+
+
+// Assignment 2:
+const aboveAverageFunction = (data) => {
+
+}
+
